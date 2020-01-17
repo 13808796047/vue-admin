855b56b2e3a14437946c7f373c7d740e3763685a
Summer <summer@SummerdeMacBook-Air.local>
2020-01-17 23:33:44 +0800

    登录tab切换

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,16 +1,54 @@
 <template>
   <div id="login">
-    这里是登录页
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          :class="{ current: isActive == index }"
+          v-for="(item, index) in menuTab"
+          :key="index"
+          @click="isActive = index"
+        >
+          {{ item.text }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "login",
+  data: _ => ({
+    menuTab: [{ text: "登录" }, { text: "注册" }],
+    isActive: 0
+  }),
+  created() {},
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background: #344a5f;
+  .login-wrap {
+    width: 330px;
+    margin: auto;
+    .menu-tab {
+      text-align: center;
+      li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: #fff;
+        border-radius: 2px;
+        cursor: pointer;
+      }
+      .current {
+        background: rgba(0, 0, 0, 0.1);
+      }
+    }
+  }
 }
 </style>
